6c2271ff6151986822ef3f61eb91149639e3f0b3
xavierpaulus <xavier.paulus@gmail.com>
2019-09-01 17:14:50 +0200

    php in public

--- public/test.php
+++ public/test.php
@@ -1,5 +1,9 @@
-<?php 
-
-echo("hello world");
-
-?>
+<html>
+    <head>
+        <title>hello</title>
+    </head>
+    <body>
+        <h1>this is a test Xavier P</h1>
+        <div><?php echo("this is my first php app")?></div>
+    </body>
+</html>
